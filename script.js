26e45394e28507b0e850f5712bd9929fcc94ba69
AIdjIO <ai.dj@pm.me>
2024-11-09 21:38:53 +0000

    gradient color for particles

--- script.js
+++ script.js
@@ -3,20 +3,35 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.fillStyle = 'white';
 console.log(ctx);
+const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
+gradient.addColorStop(0, 'white');
+gradient.addColorStop(0.5, 'magenta');
+gradient.addColorStop(1, 'blue');
+ctx.fillStyle = gradient;
 
 class Particle {
     constructor(effect){
         this.effect = effect;
-        this.x = Math.random() * this.effect.width;
-        this.y = Math.random() * this.effect.height;
-        this.radius = 15;
+        this.radius = Math.random() * 10 + 5 ;
+        this.x = this.radius + Math.random() * ( this.effect.width - this.radius * 2);
+        this.y = this.radius + Math.random() * ( this.effect.height - this.radius * 2);
+        this.vx = Math.random() * 1 - 0.5;
+        this.vy = Math.random() * 1 - 0.5;
     }
     draw(context){
+        // context.fillStyle = 'hsl(' + this.x * 0.5 + ', 100%, 50%)';
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         context.fill();
+        context.stroke();
+    }
+    update(){
+        this.x += this.vx;
+        if( this.x > this.effect.width - this.radius || this.x < this.radius ) this.vx *= -1;
+        
+        this.y+= this.vy;
+        if( this.y > this.effect.height - this.radius || this.y < this.radius ) this.vy *= -1;        
     }
 }
 
@@ -26,7 +41,7 @@ class Effect {
         this.width = this.canvas.width;
         this.height = this.canvas.height;
         this.particles = [];
-        this.numberOfParticles = 20;
+        this.numberOfParticles = 1000;
         this.createParticles();
     }
     createParticles(){
@@ -37,13 +52,17 @@ class Effect {
     handleParticles(context){
         this.particles.forEach(particle=>{
             particle.draw(context);
+            particle.update();
         })
     }
 }
 
 const effect = new Effect(canvas);
-effect.handleParticles(ctx);
 
 function animate(){
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    effect.handleParticles(ctx);
+    requestAnimationFrame(animate);
 }
+
+animate();
